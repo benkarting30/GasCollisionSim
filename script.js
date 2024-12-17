35f085d4e55e0980853bdffd42c30aae8f634831
benkarting30 <23thompsonben@colchsfc.ac.uk>
2024-12-17 09:24:15 +0000

    Go

---
+++ script.js
@@ -0,0 +1,43 @@
+let walls, particles
+const tileSize = 30
+function setup(){
+    walls = new Group()
+    walls.h = tileSize
+    walls.w = tileSize
+    walls.tile = "x"
+    walls.color = 'red'
+    walls.collider = 's'
+
+    particles = new Group()
+    particles.radius = 5
+    particles.collider = 'd'
+
+
+    new Tiles([
+            "xxxxxxxxxxxxxxx",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "x.............x",
+            "xxxxxxxxxxxxxxx"
+        ],
+    0,
+    0,
+    tileSize,
+    tileSize
+    )
+
+    for (let i = 0; i < 100; i++){
+        let particle = new particles.Sprite()
+        particle.x = floor(random(30, 30*14))
+        particle.y = floor(random(30, 30*13))
+    }
+}
